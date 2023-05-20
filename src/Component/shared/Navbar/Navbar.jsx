bcac61748c5a0a8e69e74aa86425c0076c3c9217
NafisOfficial <nafisiqbal.net2002@gmail.com>
2023-05-20 14:35:08 +0600

    add toy page design complete

--- src/Component/shared/Navbar/Navbar.jsx
+++ src/Component/shared/Navbar/Navbar.jsx
@@ -13,10 +13,10 @@ const Navbar = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Home</a></li>
-                            <li><a>All Toys</a></li>
-                            <li><a>Blogs</a></li>
-                            <li><a>My toys</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/all-toys">All Toys</Link></li>
+                            <li><Link to="/blogs">Blogs</Link></li>
+                            <li><Link to="/my-toys">My toys</Link></li>
                             <li><Link to='/add-a-toy'>Add toys</Link></li>
                         </ul>
                     </div>
