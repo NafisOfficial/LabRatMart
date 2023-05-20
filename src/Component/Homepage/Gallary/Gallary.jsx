b5e587110e45ed1beecb9d9f3baa24d299156e21
NafisOfficial <nafisiqbal.net2002@gmail.com>
2023-05-20 15:58:42 +0600

    gallary section done

--- src/Component/Homepage/Gallary/Gallary.jsx
+++ src/Component/Homepage/Gallary/Gallary.jsx
@@ -1,9 +1,135 @@
 import React from 'react';
+import './gallary.css'
 
 const Gallary = () => {
     return (
-        <div>
-            Gallary page.
+        <div className='m-6 sm:m-20 flex flex-col justify-center'>
+            <h1 className='text-center mb-6 text-4xl text-[#B4CC5F]'>Our photo gallary</h1>
+            <div className='flex justify-between text-[#aac549]'>
+                <h1>Latest Photos</h1>
+                <div className='flex items-center'><h3>Page</h3><input className='w-10 px-1 border-2 border-[#aac549]' type="number" placeholder='1' /><h3>of 5</h3></div>
+            </div>
+            <div className='grid grid-cols-1 sm:grid-cols-4  ms-22 gap-4 my-6'>
+                <div>
+                    <div><img src="https://kdvr.com/wp-content/uploads/sites/11/2022/08/1040x585-2021-0614-best-educational-toys-for-toddlers-d9c9fe.jpg?strip=1" className='customImage' alt="" /></div>
+                    <div className='flex justify-between text-[#B4CC5F]'>
+                        <p>Date:</p>
+                        <p>1550 views</p>
+                    </div>
+                </div>
+                <div>
+                    <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSD4BnZDdrKV2zD9H288XUsZIzXiXbXrMKj7Q&usqp=CAU" className='customImage'  alt="" /></div>
+                    <div className='flex justify-between text-[#B4CC5F]'>
+                        <p>Date:</p>
+                        <p>1550 views</p>
+                    </div>
+                </div>
+                <div>
+                    <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8b4xgObU_zjjPvl_d-Q-nA61nqq1UyfFj7dG9ZUtqoUQn8xrlOmA8dEI4SEWd2_o_JBY&usqp=CAU" className='customImage'  alt="" /></div>
+                    <div className='flex justify-between text-[#B4CC5F]'>
+                        <p>Date:</p>
+                        <p>1550 views</p>
+                    </div>
+                </div>
+                <div>
+                    <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKiFLWBPrzLm9K58fVTwC37fQLbUMvvEYoVw&usqp=CAU" className='customImage'  alt="" /></div>
+                    <div className='flex justify-between text-[#B4CC5F]'>
+                        <p>Date</p>
+                        <p>1550 views</p>
+                    </div>
+                </div>
+                <div>
+                    <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5yk-HI9WrWEQRcOl2SGZ9HHEEELWFDGPgdw&usqp=CAU" className='customImage'  alt="" /></div>
+                    <div className='flex justify-between text-[#B4CC5F]'>
+                        <p>Date:</p>
+                        <p>1550 views</p>
+                    </div>
+                </div>
+                <div>
+                    <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-3t7CXuadYskGlADUTkXDjHO-rsYLSl9yVA&usqp=CAU" className='customImage' alt="" /></div>
+                    <div className='flex justify-between text-[#B4CC5F]'>
+                        <p>Date:</p>
+                        <p>1550 views</p>
+                    </div>
+                </div>
+                <div>
+                    <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5yk-HI9WrWEQRcOl2SGZ9HHEEELWFDGPgdw&usqp=CAU" className='customImage' alt="" /></div>
+                    <div className='flex justify-between text-[#B4CC5F]'>
+                        <p>Date:</p>
+                        <p>1550 views</p>
+                    </div>
+                </div>
+                <div>
+                    <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGwlj_I5u3qgaNi_FUbg9JYfcFYnQeI3qkAw&usqp=CAU" className='customImage' alt="" /></div>
+                    <div className='flex justify-between text-[#B4CC5F]'>
+                        <p>Date</p>
+                        <p>1550 views</p>
+                    </div>
+                </div>
+                <div>
+                    <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREH1C9R9vOv_ArXoHbpXmLcNSNkLepJPtIag&usqp=CAU" className='customImage' alt="" /></div>
+                    <div className='flex justify-between text-[#B4CC5F]'>
+                        <p>Date:</p>
+                        <p>1550 views</p>
+                    </div>
+                </div>
+                <div>
+                    <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxY6LhhzwxJYq363CZw15_YQdgzqmvIcZxog&usqp=CAU" className='customImage' alt="" /></div>
+                    <div className='flex justify-between text-[#B4CC5F]'>
+                        <p>Date:</p>
+                        <p>1550 views</p>
+                    </div>
+                </div>
+                <div>
+                    <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScuZdvO5VXPsCsW_7MrJysIl7wjDmnYjaJYA&usqp=CAU" className='customImage' alt="" /></div>
+                    <div className='flex justify-between text-[#B4CC5F]'>
+                        <p>Date:</p>
+                        <p>1550 views</p>
+                    </div>
+                </div>
+                <div>
+                    <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSC6iCpRcvBgm2OFeJEpudxW_g-qXkED4titg&usqp=CAU" className='customImage' alt="" /></div>
+                    <div className='flex justify-between text-[#B4CC5F]'>
+                        <p>Date</p>
+                        <p>1550 views</p>
+                    </div>
+                </div>
+                <div>
+                    <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwcYsnl8yL1rlfWVCB0BKANWdb-usdAXDseQ&usqp=CAU" className='customImage' alt="" /></div>
+                    <div className='flex justify-between text-[#B4CC5F]'>
+                        <p>Date:</p>
+                        <p>1550 views</p>
+                    </div>
+                </div>
+                <div>
+                    <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYWgQb7rk4j1sSLDcnKL5EUQaoGORDpos7EA&usqp=CAU" className='customImage' alt="" /></div>
+                    <div className='flex justify-between text-[#B4CC5F]'>
+                        <p>Date:</p>
+                        <p>1550 views</p>
+                    </div>
+                </div>
+                <div>
+                    <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqzQrG2MkLN02v05yuOI6mU5iQ9wTwCEvbFQ&usqp=CAU" className='customImage' alt="" /></div>
+                    <div className='flex justify-between text-[#B4CC5F]'>
+                        <p>Date:</p>
+                        <p>1550 views</p>
+                    </div>
+                </div>
+                <div>
+                    <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_TfJKDZYD97aygmzqBEBIe6xIWIiPuaO7yw9byk3U-PjfAqboE4rzVnLYqYqFeD231OY&usqp=CAU" className='customImage' alt="" /></div>
+                    <div className='flex justify-between text-[#B4CC5F]'>
+                        <p>Date</p>
+                        <p>1550 views</p>
+                    </div>
+                </div>
+            </div>
+            <div className='flex justify-between'>
+                <button className='bg-[#B9CF6A] px-4 py-2 text-white'>Previous</button>
+                <div className='flex gap-1'>
+                    <button className='bg-[#B9CF6A] px-3'>1</button><button className='bg-[#EEEEEE] px-3'>2</button><button className='bg-[#EEEEEE] px-3'>3</button><button className='bg-[#EEEEEE] px-3'>4</button><button className='bg-[#EEEEEE] px-3'>5</button>
+                </div>
+                <button className='bg-[#B9CF6A] px-4 py-2 text-white'>Next</button>
+            </div>
         </div>
     );
 };
